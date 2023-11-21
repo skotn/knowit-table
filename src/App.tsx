49bcb8c197cadefeadc506dd45ff64ac4e3339f5
skotn <mat.skotn@live.no>
2023-11-21 13:53:37 +0100

    cleaned up App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Repository } from "./models/repository";
+import Table from "./components/Table";
+import Pagination from "./components/Pagination";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
 
-function App() {
+export default function App() {
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const [page, setPage] = useState<number>(0);
+
+  useEffect(() => {
+    fetch(
+      `https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100`,
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response.items);
+        setRepos(response.items || []);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {repos.length > 0 ? (
+        <div className="container">
+          <Table repos={repos.slice(page, page + 20)} />
+          <Pagination page={page} setPage={setPage} />
+        </div>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
-
-export default App;
