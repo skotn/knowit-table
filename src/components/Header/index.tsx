bceb743a4ec395d2607499a4f6b447626fdfc31e
skotn <mat.skotn@live.no>
2023-11-21 13:52:28 +0100

    Seperate loader and header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      Popular JavaScript-repositories on GitHub
+    </header>
+  );
+}
---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,6 @@
+import logo from "./../../logo.svg";
+import "./Loader.css";
+
+export default function Loader() {
+  return <img src={logo} className="app-logo" alt="logo" />;
+}
